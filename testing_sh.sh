e5d537c9e7e253dc44cd6ddd524afd733f6d1642
LV6 <63260147+LV6LV@users.noreply.github.com>
2024-03-24 09:47:43 -0500

    Update testing_sh.sh

--- testing_sh.sh
+++ testing_sh.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Send the command to the server
-response=$(curl <site>)
+# Send the command to the server and save the response
+response=$(curl "<site>")
 
-# Parse each AWS credential from the stdout
-# Assuming the structure of the line is similar to: <li>Access Key: AKIA...</li>
-AWS_ACCESS_KEY_ID=$(echo "$response" | grep 'Access Key:' | awk -F'<' '{print $1}' | awk -F': ' '{print $2}')
+# Parse AWS Access Key ID
+# The grep searches for 'Access Key', then awk prints everything after "Access Key: "
+AWS_ACCESS_KEY_ID=$(echo "$response" | grep 'Access Key:' | awk -F'Access Key: ' '{print $2}' | cut -d '<' -f 1)
 
-# Assuming the structure of the line is similar to: <li>Secret Key: paVI8VgTWkPI3jDNkdzUMvK4CcdXO2T7sePX0ddF</li>
-AWS_SECRET_ACCESS_KEY=$(echo "$response" | grep 'Secret Key:' | awk -F'<' '{print $1}' | awk -F': ' '{print $2}')
+# Parse AWS Secret Access Key
+# Similar to the Access Key, but searching for 'Secret Key' and extracting the part after it
+AWS_SECRET_ACCESS_KEY=$(echo "$response" | grep 'Secret Key:' | awk -F'Secret Key: ' '{print $2}' | cut -d '<' -f 1)
 
 echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
 echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
